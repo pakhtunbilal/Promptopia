8068a0a29fcde023b45ddf8e0d8824c4c0fd485c
pakhtunbilal <microbilalpthn@gmail.com>
2023-06-15 17:54:24 +0530

    thirteenth

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -4,7 +4,7 @@ import Prompt from "@models/prompt";
 export const GET = async (req)=>{
     try {
         await connectToDB();
-        const prompts = await Prompt.find({}).populate('creater')
+        const prompts = await Prompt.find({}).populate('creator')
         return new Response(JSON.stringify(prompts), {status: 200})
     } catch (error) {
         return new Response("Failed to fetvh all the prompts.", {status: 500})
--- app/api/users/[id]/posts/route.js
+++ app/api/users/[id]/posts/route.js
@@ -5,8 +5,8 @@ export const GET = async (req, {params})=>{
     try {
         await connectToDB();
         const prompts = await Prompt.find({
-            creater: params.id
-        }).populate('creater')
+            creator: params.id
+        }).populate('creator')
         return new Response(JSON.stringify(prompts), {status: 200})
     } catch (error) {
         return new Response("Failed to fetvh all the prompts.", {status: 500})
